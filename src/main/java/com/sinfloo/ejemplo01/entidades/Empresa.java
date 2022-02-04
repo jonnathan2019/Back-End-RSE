0813ea867cf8c0d883a4359e0a08da1b1b6b3748
Jonnathan <juanperezxrl82000@gmail.com>
2022-02-04 05:03:33 -0500

    postgress-2

--- src/main/java/com/sinfloo/ejemplo01/entidades/Empresa.java
+++ src/main/java/com/sinfloo/ejemplo01/entidades/Empresa.java
@@ -2,6 +2,7 @@
 package com.sinfloo.ejemplo01.entidades;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.Date;
 import java.util.List;
 import javax.persistence.*;
 
@@ -10,20 +11,44 @@ import javax.persistence.*;
 public class Empresa {
     
     @Id
-    @Column (name = "Empresa_ID")
+    @Column(name = "Empresa_ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int Empresa_ID;
-    @Column (name = "nombre")
+
+    @Column(name = "nombre")
     private String nombre;
-    @Column (name = "sector_tipo")
-    private String sector_tipo;
-    @Column (name = "numero_empleados")
-    private int numero_empleados;
-    @Column (name = "RUC_empresa")
-    private String RUC_empresa;
+
+    @Column(name = "sitio_web")
+    private String sitio_web;
+
+    @Column(name = "ciudad_operacion")
+    private String ciudad_operacion;
     
+    @Column(name = "direccion_operacion")
+    private String direccion_operacion;
+
+    @Column(name = "sector_tipo")
+    private String sector_tipo;
+    
+    @Column(name = "numero_sedes")
+    private int numero_sedes;
+    
+    @Column(name = "numero_empleados")
+    private int numero_empleados;
+    
+    @Column(name = "RUC_empresa")
+    private String RUC_empresa;
+
+    @Column(name = "fecha_inicio_operciones")
+    private Date fecha_inicio_operciones;
+    
+    @Column(name = "estimado_ingresos")
+    private int estimado_ingresos;
+
+    
+
     @JsonIgnore
-    @OneToOne (mappedBy = "empresa")
+    @OneToOne(mappedBy = "empresa")
     private Encuestado encuestado;
 
     public int getEmpresa_ID() {
@@ -33,7 +58,7 @@ public class Empresa {
     public void setEmpresa_ID(int Empresa_ID) {
         this.Empresa_ID = Empresa_ID;
     }
-    
+
     public String getNombre() {
         return nombre;
     }
@@ -72,6 +97,54 @@ public class Empresa {
 
     public void setEncuestado(Encuestado encuestado) {
         this.encuestado = encuestado;
+    }
+
+    public String getSitio_web() {
+        return sitio_web;
+    }
+
+    public void setSitio_web(String sitio_web) {
+        this.sitio_web = sitio_web;
+    }
+
+    public String getCiudad_operacion() {
+        return ciudad_operacion;
+    }
+
+    public void setCiudad_operacion(String ciudad_operacion) {
+        this.ciudad_operacion = ciudad_operacion;
+    }
+
+    public String getDireccion_operacion() {
+        return direccion_operacion;
+    }
+
+    public void setDireccion_operacion(String direccion_operacion) {
+        this.direccion_operacion = direccion_operacion;
+    }
+
+    public int getNumero_sedes() {
+        return numero_sedes;
+    }
+
+    public void setNumero_sedes(int numero_sedes) {
+        this.numero_sedes = numero_sedes;
+    }
+
+    public Date getFecha_inicio_operciones() {
+        return fecha_inicio_operciones;
+    }
+
+    public void setFecha_inicio_operciones(Date fecha_inicio_operciones) {
+        this.fecha_inicio_operciones = fecha_inicio_operciones;
+    }
+
+    public int getEstimado_ingresos() {
+        return estimado_ingresos;
+    }
+
+    public void setEstimado_ingresos(int estimado_ingresos) {
+        this.estimado_ingresos = estimado_ingresos;
     }
     
     
